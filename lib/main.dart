27703eff004c3a9948f7da50ec380c9822e27a66
Yizhou Mao <uohziyoam@gmail.com>
2020-02-08 03:35:34 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:MultitaskResearch/instruction.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var widthRatio = MediaQuery.of(context).size.width / 768;
+    var heightRatio = MediaQuery.of(context).size.height / 1024;
+    return Scaffold(body: Instruction(isInstruction: true));
+  }
+}
